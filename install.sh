832fcf39f517ab20bd065d65fc672914284e1328
zb3k <d.bourim@gmail.com>
2021-12-12 01:44:06 +0300

    wip

--- install.sh
+++ install.sh
@@ -1,192 +1,144 @@
 #!/bin/bash
 
-# DEBUG=1
-
+DEBUG=yes
+WAIT_SECONDS=0.5
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PRIVATE_DIR=$SCRIPT_DIR/private
+
+
+# ------------------------------------------------------------------------------
+# Output helpers
+# ------------------------------------------------------------------------------
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
-PURPLE='\033[1;35m'
+PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
-GRAY='\033[0;37m'
+GREY='\033[0;37m'
 NC='\033[0m'
 
-header() {
-    printf "$PURPLE\n$1\n==================================================\n$NC"
-}
+print_header() { echo -e "$ORANGE##########[ $1 ]##########$NC"; sleep $WAIT_SECONDS; }
+print_success() { echo -e "$GREEN[ SUCCESS ]$NC"; }
+print_skipping() { echo -e "$CYAN[ SKIPPING ]$NC"; }
 
-debug() {
-	if [[ $DEBUG ]]; then
-		printf "   $RED[ $1 ] $BLUE$2 $ORANGE$3 $CYAN$4 $NC\n"
-	fi
-}
-
-updateSystem() {
-	header "Update system"
-	sudo pacman --noconfirm -Syu
-}
-
-installXorg() {
-	# install base-devel if not installed
-	sudo pacman -S --noconfirm --needed base-devel wget git
-
-	# choose video driver
-	printf "[0] No (default)\n[1] Intel\n[2] AMD\n[3] NVidia\n"
-	read -r -p "Install video card driver: " DRV_TYPE
-
-	case $DRV_TYPE in
-		[0][*])
-			DRIVER_PACKAGES=""
-			;;
-		[1])
-			DRIVER_PACKAGES='xf86-video-intel'
-			;;
-		[2])
-			DRIVER_PACKAGES='xf86-video-amdgpu'
-			;;
-		[3])
-			DRIVER_PACKAGES='nvidia nvidia-settings nvidia-utils'
-			;;
-	esac
-	
-	# install xorg if not installed
-	sudo pacman -S --noconfirm --needed xorg xorg-xinit xorg-xinput $DRIVER_PACKAGES
-}
-
-# # install fonts
-# mkdir -p ~/.local/share/fonts
-# mkdir -p ~/.srcs
-
-# cp -r ./fonts/* ~/.local/share/fonts/
-# fc-cache -f
-# clear 
-
-
-# title="kizu's rice setup script"
-# prefix="[\033[;32m*\033[0m]"
-
-# downloadDependencies() {
-#     if grep "Arch\|Artix\|EndeavourOS\|Manjaro" /etc/*-release; then
-#         clear 
-#         echo -e "$prefix Running an system update..."
-#         sudo pacman --noconfirm -Syu
-
-#         mkdir -p $HOME/.setup-scripto
-#         sleep 0.5
-#         clear
-
-#         if [[ -e /usr/bin/paru ]]; then
-#             echo -e "$prefix paru detected. Installing dependencies..."
-#             paru -S bspwm sxhkd rofi polybar neovim alacritty dunst picom brightnessctl playerctl dunst feh maim viewnior jq xclip imagemagick bsp-layout i3lock-color
-#         elif [[ -e /usr/bin/yay ]]; then
-#             echo -e "$prefix yay detected. Installing dependencies..."
-#             yay -S bspwm sxhkd rofi polybar neovim alacritty dunst picom brightnessctl playerctl dunst feh maim viewnior jq xclip imagemagick bsp-layout i3lock-color
-#         else
-#             # Line from https://github.com/Axarva/dotfiles-2.0/blob/9f0a71d7b23e1213383885f2ec641da48eb01681/install-on-arch.sh#L67
-#             read -r -p "Would you like to install yay? [Y/n]: " yay
-#             sleep 1.0
-
-#             case $yay in
-#                 [yY][*])
-#                     git clone https://aur.archlinux.org/yay.git $HOME/.setup-scripto
-#                     (cd $HOME/.setup-scripto && makepkg -si)
-
-#                     echo -e "$prefix yay installed. Installing dependencies..."
-#                     yay -S bspwm sxhkd rofi polybar neovim alacritty picom brightnessctl playerctl dunst feh maim viewnior jq xclip imagemagick bsp-layout i3lock-color
-#                     ;;
-#                 [nN])
-#                     echo -e "$prefix Okay. Will not install yay."
-#                     ;;
-#             esac 
-#         fi
-
-#         sleep 1
-#     else
-#         clear
-#         echo -e "$prefix Not on a Arch based system. Failed to download dependencies. Please manually install it."
-
-#         sleep 1
-#     fi
-# }
-
+debug() { [ $DEBUG == "yes" ] && echo -e "$GREY[ DEBUG ] $RED[ $1 ] $BLUE$2 $ORANGE$3 $CYAN$4 $NC"; }
 
 # ------------------------------------------------------------------------------
-# COMMON
+# Common helpers
 # ------------------------------------------------------------------------------
 
-
-
-symlink() {
-	rm -rf $2
-	ln -s $1 $2
-	debug "symlink" $1 $2
+copy_or_link() {
+	debug "copy_or_link" $1 $2
+	# rm -rf $2
+	# ln -s $1 $2
+	# cp -r $1 $2
 }
 
-link_dotfiles_to() {
-	local DOTFILES_DIR="$1"
+copy_or_link_dir() {
+	debug "copy_or_link_dir" $1 $2
+	local SRC_DIR="$1"
 	local TARGET_DIR="$2"
-
 	mkdir -p $TARGET_DIR
-
-	debug "link_dotfiles_to" $1 $2
-
-	for file in `ls -A $DOTFILES_DIR`; do
-		local srcFile="$DOTFILES_DIR/$file"
-		local targetFile="$TARGET_DIR/$file"
-		if [[ $file == '.config' ]]; then
-			link_dotfiles_to $srcFile "$targetFile"
+	for file in `ls -A $SRC_DIR`; do
+		local SRC="$SRC_DIR/$file"
+		local TARGET="$TARGET_DIR/$file"
+		if [ -f $SRC ]; then
+			copy_or_link $SRC $TARGET
 		else
-			symlink $srcFile $targetFile
+			copy_or_link_dir $SRC $TARGET
 		fi
 	done
 }
 
 # ------------------------------------------------------------------------------
-# PUBLIC
+# Install functions
 # ------------------------------------------------------------------------------
 
-# updateSystem
+update_system() {
+	print_header "Update system"
+	sudo pacman --noconfirm -Syu
+	print_success
+}
+
+install_xorg() {
+	print_header "Install xorg"
+	echo -e "[0] No (default)\n[1] Intel\n[2] AMD\n[3] NVidia\n"
+	read -r -p "Install video card driver: " DRV_TYPE
+	case $DRV_TYPE in
+		[0][*]) DRIVER_PACKAGES="" ;;
+		[1]) DRIVER_PACKAGES='xf86-video-intel'  ;;
+		[2]) DRIVER_PACKAGES='xf86-video-amdgpu' ;;
+		[3]) DRIVER_PACKAGES='nvidia nvidia-settings nvidia-utils' ;;
+	esac
+	# sudo pacman -S --noconfirm --needed xorg xorg-xinit xorg-xinput $DRIVER_PACKAGES
+	sudo pacman -S --noconfirm --needed xorg-server xorg-utils xorg-apps $DRIVER_PACKAGES
+	print_success
+}
+
+install_aur() {
+	print_header 'Install AUR helper (yay)'
+	if [ -e /usr/bin/yay ]; then
+		print_skipping
+	else
+		rm -rf /tmp/aur_install
+		sudo pacman --noconfirm --needed -S go
+		git clone https://aur.archlinux.org/yay.git /tmp/aur_install
+		cd /tmp/aur_install 
+		makepkg -si --noconfirm 
+		cd $SCRIPT_DIR 
+		rm -rf /tmp/aur_install
+	fi
+}
+
+install_packages() {
+	print_header "Install packages"
+	local FIRST_PACKAGE=$(head -n 1 $1)
+	if [ -e /usr/bin/$FIRST_PACKAGE ]; then
+		print_skipping
+	else
+		yay --noconfirm --needed -S - < $1
+		print_success
+	fi
+}
+
+install_dotfiles() {
+	print_header "Install dotfiles : $1"
+	copy_or_link_dir "$SCRIPT_DIR/$1" $HOME	
+}
+
+prepare_storage() {
+	print_header "Prepare storage"
+	local STORAGE_DIR=$1
+	mkdir -p $STORAGE_DIR
+	mkdir -p $STORAGE_DIR/.config
+	mkdir -p $STORAGE_DIR/.fonts
+	mkdir -p $STORAGE_DIR/.scripts
+	print_success
+}
+
+finalize() {
+	print_header "Finalize"
+	fc-cache
+	print_success
+}
 
 # ------------------------------------------------------------------------------
-# Install AUR helper if needed
+# INSTALL
 # ------------------------------------------------------------------------------
-if [ ! -e /usr/bin/yay ]; then
-    header 'Install AUR helper (yay)'
-	rm -rf /tmp/aur_install
-	sudo pacman --noconfirm --needed -S go
-	git clone https://aur.archlinux.org/yay.git /tmp/aur_install
-	cd /tmp/aur_install 
-	makepkg -si --noconfirm 
-	cd $SCRIPT_DIR 
-	rm -rf /tmp/aur_install
-fi
 
-# ------------------------------------------------------------------------------
-# Install packages
-# ------------------------------------------------------------------------------
-header "Install packages"
-if [ ! -e /usr/bin/bspwm ]; then
-yay --noconfirm --needed -S\
-	picom-ibhagwan-git\
-	bspwm\
-	sxhkd\
-	rofi\
-	dunst\
-	polybar\
-	nitrogen\
-	kitty\
-	jq
-fi
+# update_system
 
-# ------------------------------------------------------------------------------
-# Link config
-# ------------------------------------------------------------------------------
-header "Link configs"
-link_dotfiles_to "$SCRIPT_DIR/theme" $HOME
+# install_xorg
 
-# ------------------------------------------------------------------------------
-# Finalize
-# ------------------------------------------------------------------------------
-header "Finalize"
-fc-cache
+# install_aur
+
+# install_packages $SCRIPT_DIR/packages.txt
+
+# prepare_storage $PRIVATE_DIR
+
+install_dotfiles "public"
+
+
+# finalize
