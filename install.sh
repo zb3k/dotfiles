1e8a392e4dcd15760626ae12140201e72bb31935
zb3k <d.bourim@gmail.com>
2023-06-10 21:43:35 +0300

    trash

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ install_packages "packages/system-bluetooth.txt"
 install_packages "packages/wm.txt"
 install_packages "packages/wm-tools.txt"
 
+install_packages "packages/trash.txt"
+
 install_dotfiles "public"
 
 exec_script "postinstall.sh"
